44b8ebc45bc5f9d134c999afdaf67e07e3dcfd20
Piyush Mehta <pmehta@austincc.edu>
2021-07-24 00:35:43 -0500

    increased api fetch delay

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -10,7 +10,7 @@ const sleep = (time) => new Promise((res) => setTimeout(res, time));
 export async function fetch(url) {
   const [, search = ""] = url.split("?");
   const params = new URLSearchParams(search);
-  await sleep(400);
+  await sleep(800);
   // const symbols = new Set((params.get("symbols") || "").split(","));
   const base = params.get("base");
   const rates = exchangeRates[base];
