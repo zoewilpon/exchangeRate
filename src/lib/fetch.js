c6632c8b2ff4e21ba9581d070e6c4db3db2b3f25
Zoe Wilpon <zoe.wilpon@gmail>
2023-07-08 13:24:24 -0500

    Revert "increased api fetch delay"
    This reverts commit 44b8ebc45bc5f9d134c999afdaf67e07e3dcfd20.

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -10,7 +10,7 @@ const sleep = (time) => new Promise((res) => setTimeout(res, time));
 export async function fetch(url) {
   const [, search = ""] = url.split("?");
   const params = new URLSearchParams(search);
-  await sleep(800);
+  await sleep(400);
   // const symbols = new Set((params.get("symbols") || "").split(","));
   const base = params.get("base");
   const rates = exchangeRates[base];
